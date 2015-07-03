014c695661333c85869186ec5e2a66dd54b1743b
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-07-02 21:02:00 -0400

    fixing for iphone

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-	$('button').click(function(){
-		var toAdd = $("input[name=message]").val();
-		$('.list1 ul').append("<li>"+toAdd+"</li>");
-	});
-	$('.list1 ul').on("click","li",function(event){
-		$('.list2 ul').append(this);
-	});
+$(document).ready(function() {
+    $('button').click(function() {
+        var toAdd = $("input[name=message]").val();
+        $('.list1 ul').append("<li>" + toAdd + "</li>");
+    });
+    $('.list1 ul').on("click", "li", function() {
+        $('.list2 ul').append(this);
+    });
 });
-
